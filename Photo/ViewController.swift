e6b916726d81eef1ee705b806f88d3f08a8ac842
ivorywang <ivory109@gmail.com>
2021-05-02 14:12:11 +0800

    Initial Commit

---
+++ Photo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Photo
+//
+//  Created by User on 2021/5/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
